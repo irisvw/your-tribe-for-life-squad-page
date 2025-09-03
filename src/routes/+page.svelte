d787df92eddf3c7bf93bdff41bf0ae3d64845001
Iris van Willigen <iris.van.willigen@gmail.com>
2025-09-03 15:05:07 +0200

    Feat: add boilerplate and list of squad members #1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let { data } = $props();
+    const members = data.members;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<ul>
+    {#each members as member}
+    <li>
+        <a href="/{member.id}">{member.name} - {member.id}
+        <img src="{member.avatar}" alt="De avatar van {member.name}"></a>
+    </li>
+{/each}
+</ul>
