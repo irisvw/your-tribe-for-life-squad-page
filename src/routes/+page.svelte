e707f17a6ca21ef6ed4d7ece9a6bb6987e7da6e9
Iris van Willigen <iris.van.willigen@gmail.com>
2025-09-04 13:06:42 +0200

    feat: add script to group members by birth month

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script>
     let { data } = $props();
     const members = data.members;
+
+    members.forEach(member => {
+        const dateString = member.birthdate;
+        const date = new Date(dateString); // converteert datum string naar Date object
+        member.birthmonth = date.toLocaleString('default', { month: 'short' }); // converteert datum naar maandnaam en slaat het op in member
+        member.birthday = date.getDate(); // converteert datum naar dag van de maand en slaat het op in member
+    });
+
+    const membersByMonth = Object.groupBy(members, ({ birthmonth }) => birthmonth);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -9,8 +18,14 @@
 <ul>
     {#each members as member}
     <li>
-        <a href="/{member.id}">{member.name} - {member.id}
-        <img src="{member.avatar}" alt="De avatar van {member.name}"></a>
+        <a href="/{member.id}">{member.name} - {member.id} - {member.birthdate}</a>
+    </li>
+    {/each}
+
+    <h2>Augustus</h2>
+    {#each membersByMonth.Aug as member}
+    <li>
+        <a href="/{member.id}">{member.name} - {member.birthday}</a>
     </li>
     {/each}
 </ul>
