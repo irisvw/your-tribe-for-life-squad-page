3e3bbde4edd403ad115c736b7d442b195dfbc93f
nadiachaja <nadiagaja13@gmail.com>
2025-09-09 13:31:23 +0200

    #8 styleguide gemaakt fonts, kleuren en border

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,23 @@
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+	:root{
+
+		--primary-font-family: "Caprasimo", serif;
+		--secondary-font-family: "Belanosima", sans-serif;
+	
+		--primary-color:#000000 ;
+		--secondary-color: #FFFFFF ;
+		--bg-general: #C2ECC0;
+		--bg-layer-1: #DAC1E4;
+		--bg-layer-2: #C1D1E4;
+		--bg-layer-3: #FAE3B8; 
+		--primary-text:#000000 ;
+		--secondary-text:#FFFFFF ; 
+
+		--border: 2px solid var(var(--primary-color))
+
+	}
+</style>
