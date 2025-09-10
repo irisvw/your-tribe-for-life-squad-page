663150c978632adcd30335d0238a5a3a210b6e25
Kim Nikita Schijf <kimnikitaschijfhva@icloud.com>
2025-09-10 19:14:58 +0200

    #41 Zoom animatie toegevoegd + basis standaard transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,62 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from "$lib/assets/favicon.png";
+  import { onNavigate } from "$app/navigation";
 
-	let { children } = $props();
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    // start de view transition en wacht op navigatie
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+  :global(::view-transition-old(root), ::view-transition-new(root)) {
+    animation-duration: 0.4s;
+    animation-timing-function: ease;
+    animation-fill-mode: both;
+  }
+
+  :global(::view-transition-old(root)) {
+    animation-name: zoomOutFade;
+  }
+
+  :global(::view-transition-new(root)) {
+    animation-name: zoomIn;
+  }
+
+  @keyframes zoomIn {
+    from {
+      transform: scale(0.9);
+      opacity: 0;
+    }
+    to {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+
+  @keyframes zoomOutFade {
+    from {
+      transform: scale(1);
+      opacity: 1;
+    }
+    to {
+      transform: scale(1.1);
+      opacity: 0;
+    }
+  }
+</style>
